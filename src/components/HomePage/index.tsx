10b02850d8bde125823fe449654523de07d7bc6f
Joe Williams-Langley <joewilliamslangley@gmail.com>
2022-10-08 13:40:50 +0100

    UI styling to form components

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import { Heading, Box } from "@cruk/cruk-react-components";
-import { useForm } from "react-hook-form";
+import { Heading, Box, TextField, Select, Button } from "@cruk/cruk-react-components";
+import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
 import { NasaSearchParams } from "../../types";
 import Results from "../Results";
 
 const schema = yup.object().shape({
-  keywords: yup.string().min(2, "keywords must have at least 2 characters.").max(50, "keywords must have at most 50 characters.").required(),
+  keywords: yup.string().min(2, "Keywords must have at least 2 characters.").max(50, "keywords must have at most 50 characters.").required(),
   yearStart: yup.number().typeError("Please enter a valid number.").integer("Please enter a valid number.").min(1900, "Year start must be after 1900.").max(new Date().getFullYear(), "Year start must not be in the future.").notRequired(),
   mediaType: yup.string().oneOf(['audio', 'video', 'image'], "Please select a media type.").required("Please select a media type.")
 });
@@ -22,7 +22,7 @@ export const HomePage = () => {
   const [params, setParams] = useState({})
   const [isSearch, setIsSearch] = useState(false)
 
-  const { register, handleSubmit, formState: { errors } } = useForm<IFormInputs>({
+  const { register, handleSubmit, control, formState: { errors } } = useForm<IFormInputs>({
     resolver: yupResolver(schema),
   });
 
@@ -42,20 +42,55 @@ export const HomePage = () => {
       <Heading h1>React Exercise</Heading>
 
       <form onSubmit={handleSubmit(submitForm)}>
+        <Controller
+          name="keywords"
+          control={control}
+          defaultValue=""
+          render={({ field: { value, onChange } }) =>
+            <TextField
+              onChange={onChange}
+              value={value}
+              label="Keywords"
+              required
+              hasError={typeof errors.keywords !== "undefined" }
+              errorMessage={errors.keywords?.message}
+            />}
+        />
 
-        <input type="text" placeholder="Keywords" {...register('keywords')}/>
-        <p>{errors.keywords?.message}</p>
-        <select {...register('mediaType')}>
-          <option disabled selected value> -- select an option -- </option>
-          <option value="audio">Audio</option>
-          <option value="video">Video</option>
-          <option value="image">Image</option>
-        </select>
-        <p>{errors.mediaType?.message}</p>
-        <input type="text" {...register('yearStart')} placeholder="Minimum year" />
-        <p>{errors.yearStart?.message}</p>
-        <input type="submit" />
+        <Controller
+          name="mediaType"
+          control={control}
+          defaultValue=""
+          render={({ field: { onChange } }) =>
+            <Select
+              onChange={onChange}
+              label="Media Type"
+              required
+              hasError={typeof errors.mediaType !== "undefined" }
+              errorMessage={errors.mediaType?.message}
+            >
+              <option disabled selected value> -- select an option -- </option>
+              <option value="audio">Audio</option>
+              <option value="video">Video</option>
+              <option value="image">Image</option>
+            </Select>
+            }
+        />
 
+        <Controller
+          name="yearStart"
+          control={control}
+          render={({ field: { value, onChange } }) =>
+            <TextField
+              onChange={onChange}
+              value={value}
+              label="Minimum Year"
+              hasError={typeof errors.yearStart !== "undefined" }
+              errorMessage={errors.yearStart?.message}
+            />}
+        />
+
+        <Button appearance="primary" type="submit">Submit</Button>
       </form>
 
       {isSearch ? <Results searchParams={params}/> : null}
