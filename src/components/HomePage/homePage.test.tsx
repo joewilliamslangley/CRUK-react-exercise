148614a276fa768f92692348f9ed363f8b328d55
Joe Williams-Langley <joewilliamslangley@gmail.com>
2022-10-15 18:58:54 +0100

    addded test to check that homepage renders form when initialised

--- src/components/HomePage/homePage.test.tsx
+++ src/components/HomePage/homePage.test.tsx
@@ -8,6 +8,24 @@ describe("Home", () => {
     render(<HomePage />);
     const heading = screen.getByRole("heading", { level: 1 });
     expect(heading).toBeInTheDocument();
-    expect(heading).toHaveTextContent("Welcome to Next JS");
+    expect(heading).toHaveTextContent("React Exercise");
   });
+
+  it("renders a form", () => {
+    render(<HomePage />);
+    const keywords = screen.getByLabelText("Keywords (required)")
+    const mediaType = screen.getByLabelText("Media Type (required)")
+    const minYear = screen.getByLabelText("Minimum Year")
+    const submit = screen.getByText("Submit")
+    expect(keywords).toBeInTheDocument()
+    expect(mediaType).toBeInTheDocument()
+    expect(minYear).toBeInTheDocument()
+    expect(submit).toBeInTheDocument()
+  })
+
+  // it("returns results with three search parameters", () => {})
+
+  // it("returns results when no minimum year is included", () => {})
+
+  // it("doesn't return results when invalid parameters are given", () => {})
 });
